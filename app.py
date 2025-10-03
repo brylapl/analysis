80b9e2c86daaca6931d516752b394d133a45b4c1
brylapl <92885606+brylapl@users.noreply.github.com>
2025-10-03 13:51:33 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Ustawienia strony
+st.set_page_config(page_title="Match Preview", layout="wide")
+
+# Wczytaj plik HTML
+with open("index.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+# Renderuj HTML
+st.components.v1.html(html_content, scrolling=True)
